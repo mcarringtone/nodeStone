860c4d2d12f115ebb4aeb657c31569c43801c2fc
Carrington <mcarringtone@gmail.com>
2022-05-23 20:22:53 -0400

    jquery

--- asset/js/custom.js
+++ asset/js/custom.js
@@ -4,3 +4,9 @@ var $thisContainer = $containerBlock[0];
 for (var i = 0; i <= 8; i++) {
   // console.log($($thisContainer).children()[i])
   var $thisInputDiv = $($($($thisContainer).children()[i]).children()[1]);
+  var $thisInputBar = $($($($thisInputDiv).children()[0]).children()[0]);
+  if (localStorage.getItem($($containerBlock.children()[i]).attr("value"))) {
+    $($thisInputDiv).val(
+      localStorage.getItem($($containerBlock.children()[i]).attr("value"))
+    );
+  }
