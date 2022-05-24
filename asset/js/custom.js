aadf3154999da94dbf51015785221e621539d0f9
Carrington <mcarringtone@gmail.com>
2022-05-23 20:22:16 -0400

    more query

--- asset/js/custom.js
+++ asset/js/custom.js
@@ -1,1 +1,6 @@
 var $containerBlock = $(".container");
+
+var $thisContainer = $containerBlock[0];
+for (var i = 0; i <= 8; i++) {
+  // console.log($($thisContainer).children()[i])
+  var $thisInputDiv = $($($($thisContainer).children()[i]).children()[1]);
